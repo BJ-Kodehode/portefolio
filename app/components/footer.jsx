4a2000d539dc5e69d8052a8c5eee461ff0177e83
BJ-Kodehode <bernt25sfj@gmail.com>
2025-06-20 10:57:31 +0200

    updated year update in code

--- app/components/footer.jsx
+++ app/components/footer.jsx
@@ -1,9 +1,10 @@
 import Link from 'next/link';
 
 const Footer = () => {
+  const currentYear = new Date().getFullYear();
   return (
     <footer className="flex justify-center items-center p-4 bg-gray-800 text-white">
-      <p>© 2025 Bernt Johansen. All rights reserved.</p>
+      <p>© {currentYear} Bernt Johansen. All rights reserved.</p>
     </footer>
   );
 };
